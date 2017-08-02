f4160cb2a1008215d8b81cd47aaccc39768f2636
Vincent <vincentsnow@gmx.com>
2017-08-01 17:59:19 -0700

    tadah

---
+++ my_dict.py
@@ -0,0 +1,9 @@
+my_dict = {"name":"Vincent", "age":"28", "CoB":"USA", "favlang":"Jquery"}
+
+def print_dict(dict):
+    print "My name is " + dict["name"] + "."
+    print "My age is " + dict["age"] + "."
+    print "My country of birth is " + dict["CoB"] + "."
+    print "My favorite language is " + dict["favlang"] + "."
+
+print_dict(my_dict)
